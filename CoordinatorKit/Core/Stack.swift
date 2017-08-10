586666590fbe006e8b315bab281786110d6c2e0e
Brendan Conron <conronb@gmail.com>
2017-08-09 17:33:24 -0700

    Added stack data structure

---
+++ CoordinatorKit/Core/Stack.swift
@@ -0,0 +1,40 @@
+//
+//  Stack.swift
+//  CoordinatorKit
+//
+//  Created by Brendan Conron on 8/9/17.
+//  Copyright © 2017 Brendan Conron. All rights reserved.
+//
+
+import Foundation
+
+public struct Stack<Element> {
+    
+    private var storage: [Element] = []
+    
+    public var isEmpty: Bool {
+        return storage.isEmpty
+    }
+    
+    public var count: Int {
+        return storage.count
+    }
+    
+    mutating public func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    mutating public func pop() -> Element? {
+        return storage.popLast()
+    }
+    
+    public func peek() -> Element? {
+        return storage.last
+    }
+    
+    mutating public func removeAll() {
+        storage.removeAll()
+    }
+    
+}
